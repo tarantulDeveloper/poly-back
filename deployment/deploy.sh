168849877570838f8ae8b8c0182e0ca0fbb88aed
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-08-09 23:10:59 +0600

    images

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,13 +2,13 @@ mvn clean package -DskipTests
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/frankfurt-google.pub \
+scp -i ~/.ssh/google-frankfurt \
     ./target/*.jar \
     beksdeveloper@34.65.179.105:~/lovz-server/server.jar
 
 echo 'Restart the server...'
 
-ssh -i  ~/.ssh/frankfurt-google.pub beksdeveloper@34.65.179.105 << EOF
+ssh -i  ~/.ssh/google-frankfurt beksdeveloper@34.65.179.105 << EOF
 pgrep java | xargs kill -9
 nohup java -jar lovz-server/server.jar &
 EOF
